ecdfed787b627a055627ed73a4309e423f6abd96
YangAo <ya1398789131@163.com>
2023-12-29 15:48:54 +0800

    feat: gateway nginx 集群

--- cloud-05-gateway/gateway-02-login-server/src/main/java/edu/yangao/controller/LoginController.java
+++ cloud-05-gateway/gateway-02-login-server/src/main/java/edu/yangao/controller/LoginController.java
@@ -1,5 +1,6 @@
 package edu.yangao.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,11 @@ import java.util.UUID;
 @RestController
 public class LoginController {
 
+    @Value("${server.port}")
+    private Integer port;
+
     @GetMapping("login")
     public String login() {
-        return UUID.randomUUID().toString();
+        return port + ": " + UUID.randomUUID();
     }
 }
