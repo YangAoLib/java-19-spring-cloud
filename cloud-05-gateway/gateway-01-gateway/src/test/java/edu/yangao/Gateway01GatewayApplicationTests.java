8016c85efa8dd9669cfa5f3dcfe67df3d405d3ac
YangAo <ya1398789131@163.com>
2024-01-03 11:28:03 +0800

    feat: gateway 路由断言配置

--- cloud-05-gateway/gateway-01-gateway/src/test/java/edu/yangao/Gateway01GatewayApplicationTests.java
+++ cloud-05-gateway/gateway-01-gateway/src/test/java/edu/yangao/Gateway01GatewayApplicationTests.java
@@ -3,11 +3,14 @@ package edu.yangao;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+
 @SpringBootTest
 class Gateway01GatewayApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(LocalDateTime.now());
     }
 
 }
--- cloud-05-gateway/gateway-02-login-server/src/main/java/edu/yangao/controller/LoginController.java
+++ cloud-05-gateway/gateway-02-login-server/src/main/java/edu/yangao/controller/LoginController.java
@@ -2,6 +2,7 @@ package edu.yangao.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -13,7 +14,7 @@ public class LoginController {
     private Integer port;
 
     @GetMapping("login")
-    public String login() {
-        return port + ": " + UUID.randomUUID();
+    public String login(@RequestParam(required = false) String name) {
+        return port + ": " + name + ": " + UUID.randomUUID();
     }
 }
